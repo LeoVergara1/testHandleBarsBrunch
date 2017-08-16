19be7fa00a6d9814f3ccf8db85d08e49acca6a93
leovergara1 <leovergara.dark@gmail.com>
2017-08-16 15:50:37 -0500

    Adding script

--- app/initialize.js
+++ app/initialize.js
@@ -2,4 +2,5 @@ document.addEventListener('DOMContentLoaded', function() {
   // do your setup here
   console.log('Initialized app');
   require('./coffee/IndexController');
+  require('./coffee/HandelBarsController');
 });
